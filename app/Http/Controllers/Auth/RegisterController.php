c3b86d5fd8ee3f82e459e78dd94717816ad3ab40
Khoiron14 <khoironarkade@gmail.com>
2018-03-01 08:43:57 +0700

    update validation rules register user

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -48,11 +48,11 @@ class RegisterController extends Controller
     protected function validator(array $data)
     {
         return Validator::make($data, [
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|max:255|unique:users|email',
-            'phone' => 'required|string|max:255|unique:users',
-            'address' => 'required|string|max:255',
-            'image' => 'required',
+            'name' => 'required|string|max:25',
+            'email' => 'required|string|max:25|unique:users|email',
+            'phone' => 'required|string|max:25|unique:users',
+            'address' => 'required|string|max:100',
+            'image' => 'required|image|mimes:jpeg,bmp,png',
             'password' => 'required|string|min:6|confirmed',
         ]);
     }
