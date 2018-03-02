3d5372a82516c3ad3cfac03a632ee59b7e88d845
Khoiron14 <khoironarkade@gmail.com>
2018-03-02 10:36:27 +0700

    delete comment in web.php

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,6 @@ Route::get('/', 'HomeController@index')->name('home');
 
 Auth::routes();
 
-// Route::get('/home', 'HomeController@index')->name('home');
-
 Route::get('/admin', 'Auth\AdminController@index')->name('admin.index');
 
 Route::prefix('user')->group(function() {
