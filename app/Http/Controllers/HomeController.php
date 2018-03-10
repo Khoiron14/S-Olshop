63c19900eec45d50712428800c526b01c551d3ed
Khoiron14 <khoironarkade@gmail.com>
2018-03-10 22:29:27 +0700

    add function search()

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,4 +27,15 @@ class HomeController extends Controller
 
         return view('home', compact('items'));
     }
+
+    public function search()
+    {
+        if (request()->q) {
+            $items = Item::search(request()->q)->paginate(9);
+
+            return view('home', compact('items'));
+        }
+
+        return redirect()->back();
+    }
 }
