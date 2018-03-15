c436301cf96bc220947463496a2a525f0d62dfb0
Khoiron14 <khoironarkade@gmail.com>
2018-03-15 14:32:16 +0700

    update const, add function isType(), and getColor() on Model Status

--- app/Models/Process/Status.php
+++ app/Models/Process/Status.php
@@ -7,13 +7,31 @@ use Illuminate\Database\Eloquent\Model;
 
 class Status extends Model
 {
-    const CONFIRMED = 1;
-    const PENDING = 2;
-    const CANCELLED = 3;
+    const SELLER_CONFIRM = 1;
+    const SYSTEM_PENDING = 2;
+    const SELLER_CANCEL = 3;
+    const SYSTEM_CANCEL = 4;
+    const USER_CANCEL = 5;
 
     public $timestamps = false;
 
     protected $guarded = [];
+
+    public function isType(string $type) : bool
+    {
+        return starts_with(strtolower($this->name), $type);
+    }
+
+    public function getColor()
+    {
+        if ($this->isType('confirmed')) {
+            return 'text-success';
+        } elseif ($this->isType('cancelled')) {
+            return 'text-danger';
+        }
+
+        return 'text-primary';
+    }
 
     public function purchases()
     {
