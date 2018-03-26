cf0cbd3f8f22a97f8a6a5660cebd28ffd3b0b465
Khoiron14 <khoironarkade@gmail.com>
2018-03-26 11:01:07 +0700

    add fillable in model User

--- app/Models/Users/User.php
+++ app/Models/Users/User.php
@@ -24,7 +24,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'phone', 'address', 'avatar', 'password',
+        'name', 'email', 'phone', 'address', 'avatar', 'password', 'active', 'activation_token'
     ];
 
     /**
