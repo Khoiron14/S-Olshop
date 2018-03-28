273441a38da7afc37ebd388ec1da9af55b14417a
Khoiron14 <khoironarkade@gmail.com>
2018-03-28 09:26:16 +0700

    add const and function on Image Model

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -6,7 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
+    const USER_DEFAULT = 'seeds/user.png';
+    const ITEM_DEFAULT = 'seeds/item.png';
+    const STORE_DEFAULT = 'seeds/store.png';
+
     protected $guarded = [];
+
+    public function isDefault() : bool
+    {
+        return $this->path == self::USER_DEFAULT || self::ITEM_DEFAULT || self::STORE_DEFAULT;
+    }
 
     public function imageable()
     {
