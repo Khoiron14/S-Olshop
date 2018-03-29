ca1629d8b0a30b8d774bf6af4b8fc520529dc8f3
Khoiron14 <khoironarkade@gmail.com>
2018-03-29 14:44:08 +0700

    update store register view

--- resources/views/store/register.blade.php
+++ resources/views/store/register.blade.php
@@ -1,29 +1,32 @@
-@extends('layouts.app')
-
-@section('content')
+@extends('layouts.app') @section('content')
 <div class="container">
     <div class="row justify-content-md-center mt-5">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">Register Store</div>
+                <div class="card-header bg-primary text-white">
+                    <h3>Store Register</h3>
+                </div>
                 <div class="card-body">
-                    <form role="form" method="POST" action="{{ route('store.store') }}" enctype="multipart/form-data">
+                    <form
+                        role="form"
+                        method="POST"
+                        action="{{ route('store.store') }}"
+                        enctype="multipart/form-data">
                         {!! csrf_field() !!}
                         <div class="form-group row">
                             <label class="col-lg-4 col-form-label text-lg-right">Name</label>
                             <div class="col-lg-6">
                                 <input
-                                        type="text"
-                                        class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}"
-                                        name="name"
-                                        value="{{ old('name') }}"
-                                        autofocus
-                                        required
-                                >
+                                    type="text"
+                                    class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}"
+                                    name="name"
+                                    value="{{ old('name') }}"
+                                    autofocus="autofocus"
+                                    required="required">
                                 @if ($errors->has('name'))
-                                    <div class="invalid-feedback">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </div>
+                                <div class="invalid-feedback">
+                                    <strong>{{ $errors->first('name') }}</strong>
+                                </div>
                                 @endif
                             </div>
                         </div>
@@ -34,17 +37,16 @@
                             <div class="col-lg-6">
                                 <span>{{ request()->root() }}/</span>
                                 <input
-                                        type="text"
-                                        class="form-control{{ $errors->has('domain') ? ' is-invalid' : '' }}"
-                                        name="domain"
-                                        value="{{ old('domain') }}"
-                                        autofocus
-                                        required
-                                >
+                                    type="text"
+                                    class="form-control{{ $errors->has('domain') ? ' is-invalid' : '' }}"
+                                    name="domain"
+                                    value="{{ old('domain') }}"
+                                    autofocus="autofocus"
+                                    required="required">
                                 @if ($errors->has('domain'))
-                                    <div class="invalid-feedback">
-                                        <strong>{{ $errors->first('domain') }}</strong>
-                                    </div>
+                                <div class="invalid-feedback">
+                                    <strong>{{ $errors->first('domain') }}</strong>
+                                </div>
                                 @endif
                             </div>
                         </div>
@@ -57,6 +59,12 @@
                             </div>
                         </div>
                     </form>
+                    <small>
+                        <ul>
+                            <li>Can't change, please check again!</li>
+                            <li>You can edit avatar and description in store edit page after registered.</li>
+                        </ul>
+                    </small>
                 </div>
             </div>
         </div>
