7f9408984add8d43254b00e671c8e3b6c7654285
Khoiron14 <khoironarkade@gmail.com>
2018-04-03 14:16:17 +0700

    create comment controller and request

---
+++ app/Http/Controllers/Users/CommentController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Http\Controllers\Users;
+
+use App\Models\Shops\Store;
+use App\Models\Users\Comment;
+use App\Models\Shops\Items\Item;
+use App\Http\Requests\CommentRequest;
+use App\Http\Controllers\Controller;
+
+class CommentController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  App\Http\Requests\CommentRequest  $request
+     * @param  App\Models\Shops\Store  $store
+     * @param  App\Models\Shops\Items\Item  $item
+     * @return \Illuminate\Http\Response
+     */
+    public function store(CommentRequest $request, Store $store, Item $item)
+    {
+        $item->comments()->create([
+            'user_id' => auth()->user()->id,
+            'message' => $request->message
+        ]);
+
+        return redirect()->back();
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  App\Http\Requests\CommentRequest  $request
+     * @param  App\Models\Shops\Store  $store
+     * @param  App\Models\Shops\Items\Item  $item
+     * 
+     * @return \Illuminate\Http\Response
+     */
+    public function update(CommentRequest $request, Store $store, Item $item, Comment $comment)
+    {
+        $comment->update([
+            'message' => $request->message
+        ]);
+
+        return redirect()->back();
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  App\Http\Requests\CommentRequest  $request
+     * @param  App\Models\Shops\Store  $store
+     * @param  App\Models\Shops\Items\Item  $item
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(CommentRequest $request, Store $store, Item $item, Comment $comment)
+    {
+        $comment->destroy();
+
+        return redirect()->back();
+    }
+}
