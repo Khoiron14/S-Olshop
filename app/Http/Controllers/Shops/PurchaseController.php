508b8a88e5527dd42fa64cb03e742103085c22dc
Khoiron14 <khoironarkade@gmail.com>
2018-04-05 11:30:56 +0700

    use middleware user.hasAddress

--- app/Http/Controllers/Shops/PurchaseController.php
+++ app/Http/Controllers/Shops/PurchaseController.php
@@ -14,6 +14,7 @@ class PurchaseController extends Controller
     public function __construct()
     {
         $this->middleware('auth');
+        $this->middleware('user.hasAddress')->only('store');
     }
 
     public function store(Item $item)
